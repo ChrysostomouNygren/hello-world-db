4563609218b740bab629f45a7434bcc0f297850f
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2022-08-17 14:31:17 +0200

    det är för varmt ute, jag kan inte tänka längre

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 const app = require("express")();
 
 const config = require("./knexfile.js");
-const knex = require("knex")(config[process.env.NODE_ENV])
+const knex = require("knex")(config[process.env.NODE_ENV]);
 
 app.get("/", (req, res) => {
   res.send(process.env.GREETING);
-})
+});
 
-app.get("/add/:name", (req, res) => {
+app.get("/add/:name", async (req, res) => {
   // Ersätt person med den skapade personen från databasen
-  const person = {
-    id: 0,
-    name: "Ryan"
-  }; 
+  // const person = {
+  //   id: 0,
+  //   name: "Ryan",
+  // };
 
-  res.send(person)
-})
+  const person = await knex("people").insert({ name: req.params.name });
 
-app.get("/list", (req, res) => {
+  res.send(person);
+});
+
+app.get("/list", async (req, res) => {
   // Ersätt people med alla personer från databasen
-  const people = [{
-    id: 0,
-    name: "Ryan"
-  }];
+  // const people = [
+  //   {
+  //     id: 0,
+  //     name: "Ryan",
+  //   },
+  // ];
+  const people = await knex("people").select();
 
   res.send(people);
-})
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`)
-})
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
